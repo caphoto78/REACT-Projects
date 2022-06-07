ff759bb624d514e2f00d84ee56dd61f1f66ff6e4
caphoto78 <54139979+caphoto78@users.noreply.github.com>
2022-06-07 11:12:45 +0300

    Delete indecision-app directory

--- indecision-app/src/playground/es6-arrow-function.js
+++
@@ -1,7 +0,0 @@
-const square = function (x) {
-    return x * x
-}
-
-const squareArrow = x => x * x
-
-console.log(squareArrow(8));
--- indecision-app/src/playground/person.js
+++
@@ -1,8 +0,0 @@
-
-const isAdult = (a) => a >= 18;
-const canDrink = (b) => b >= 21;
-const isSenior = (age) => age >= 65;
-
-export { isAdult, canDrink, isSenior as default }
-
-// export default (age) => age >= 65;
